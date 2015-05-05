f873df258e12bcab67a9e224bbb96b708cea8b9a
Daniel Moura de Oliveira <paladinozero@hotmail.com>
2015-05-05 17:29:24 -0300

    versao 2.0

--- Atividade_07/1.c
+++ Atividade_07/1.c
@@ -1,75 +1,74 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+
 struct lista{
-      int dado; 
-      struct lista *prox; 
-       };
+int dado;
+struct lista *prox;
+};
 typedef struct lista Lista;
- 
- Lista* raiz;
- 
- cria_lista(){ 
-   
-   raiz=NULL;
-  
-   
-      }
-inserir(int x){
-     Lista *atual;
-     atual=(Lista*)malloc(sizeof(Lista));
-     atual->dado=x;
-     atual->prox=raiz;
-     raiz=atual;   
-     }       
 
+Lista* cria_lista(){
+	return NULL;
+}
 
-percorrer(){
-   Lista* atual;
-   for(atual=raiz;atual!=NULL;atual=atual->prox){
-        printf("%d\n",atual->dado);
-        
-    }  
-            
-            }
-lista_vazia(){               
-    if(raiz==NULL) printf("lista vazia\n"); 
-    else printf("lista nao vazia\n");              
-                   
-                   }
-busca(int x){
-     Lista* atual;
-     atual=raiz;
-     while (atual!=NULL && atual->dado!=x) atual=atual->prox;  
-     if (atual->dado==x) printf("Elemento encontrado\n");      
-             }
-remover(){
-     Lista* apaga;
-     apaga=raiz;
-     raiz=apaga->prox;
-     free(apaga);          
-               
-               }
-libera(){
-     free(raiz);
-     raiz=NULL;    
-         
-         }
+Lista* inserir(Lista* r,int x){
+	Lista* head;
+	head=(Lista*)malloc(sizeof(Lista));
+	head->dado=x;
+	head->prox=r;
+	return head;	
+}
+
+percorrer(Lista* raiz){
+Lista* atual;
+for(atual=raiz;atual!=NULL;atual=atual->prox){
+printf("%d\n",atual->dado);
+       } 
+}
+
+lista_vazia(Lista* raiz){
+if(raiz==NULL) printf("lista vazia\n");
+else printf("lista nao vazia\n");
+}
+
+Lista* busca(Lista* raiz,int x){
+Lista* atual;
+atual=raiz;
+while (atual->dado!=x && atual!=NULL) atual=atual->prox;
+if (atual->dado==x) printf("Elemento encontrado\n");
+else printf("Elemento nao encontrado\n");
+}
+
+Lista* remover(Lista* raiz){
+
+Lista* apaga;
+apaga=raiz;
+raiz=apaga->prox;
+free(apaga);
+return raiz;
+
+}
+
+Lista* libera(Lista* raiz){
+free(raiz);
+raiz=NULL;
+return raiz;
+}
 
 main(){
- 
-  cria_lista();
-  inserir(10);
-  inserir(20);
-  inserir(30);
-  inserir(40);
-  lista_vazia();
-  busca(20);
-  percorrer();
-  remover();
-  percorrer();
-  libera();
-     
-       
-       
-       }
+	Lista* raiz;
+	raiz=cria_lista();
+	lista_vazia(raiz);
+   	raiz=inserir(raiz,2);
+   	raiz=inserir(raiz,3);
+   	raiz=inserir(raiz,4);
+   	raiz=inserir(raiz,5);
+   	percorrer(raiz);
+   	lista_vazia(raiz);
+   	busca(raiz,4);
+      raiz=remover(raiz);
+      percorrer(raiz);
+      raiz=libera(raiz);
+   	lista_vazia(raiz);  	
+}
